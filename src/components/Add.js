e35b06d7327b46b591cbab6ea75b07f0decb171c
yumeyuniko <daigocafe@msn.com>
2022-01-30 00:59:17 +0900

    検索結果の画像を表示するためのコンポーネント作成

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ResultCard } from './ResultCard';
 
 export const Add = () => {
   const [query, setQuery] = useState('');
@@ -39,7 +40,9 @@ export const Add = () => {
           {results.length > 0 && (
             <ul className="results">
               {results.map((movie) => (
-                <li>{movie.title}</li>
+                <li key={movie.id}>
+                  <ResultCard  movie={movie} />
+                </li>
               ))}
             </ul>
           )}
