10a2a1367e9f4cdcf26fb1afb5c1fe6cd701ecb4
yumeyuniko <daigocafe@msn.com>
2022-01-30 01:14:25 +0900

    release-dateにsubstringを指定して西暦年だけ表示

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -17,7 +17,7 @@ export const ResultCard = ({movie}) => {
       <div className="info">
         <div className="header">
           <h3 className="title">{movie.title}</h3>
-          <h4 className="release-date">{movie.release_date}</h4>
+          <h4 className="release-date">{movie.release_date.substring(0,4)}</h4>
         </div>
       </div>
     </div>
